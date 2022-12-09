c555205b3b7b06c7add102f6f5f85260cb98fd58
David <david.programming@protonmail.com>
2022-12-09 18:03:15 +0100

    fix: add the id to the DTO which is needed in the frontend

--- src/main/java/de/davidkoehlmann/ecommerceapplicationbackend/product/ProductDTO.java
+++ src/main/java/de/davidkoehlmann/ecommerceapplicationbackend/product/ProductDTO.java
@@ -9,6 +9,7 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 public class ProductDTO {
+    private Long id;
     private String name;
     private BigDecimal price;
     private String description;
