f02d04b0dc0b38e470a3fd7e8367437315e8ea8a
David <david.programming@protonmail.com>
2024-03-12 20:23:38 +0100

    test: product boilerplate

---
+++ src/test/java/de/davidkoehlmann/ecommerceapplicationbackend/product/ProductServiceImplTest.java
@@ -0,0 +1,33 @@
+package de.davidkoehlmann.ecommerceapplicationbackend.product;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceImplTest {
+
+    @Test
+    void getProductByIdTest() {
+        // TODO: create test
+    }
+
+    @Test
+    void getAllProducts() {
+        // TODO: create test
+    }
+
+    @Test
+    void createProduct() {
+        // TODO: create test
+    }
+
+    @Test
+    void updateProduct() {
+        // TODO: create test
+    }
+
+    @Test
+    void deleteProduct() {
+        // TODO: create test
+    }
+}
